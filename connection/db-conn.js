6a48afeda6e9b6f3c78c3a1c90374dc9c6dcacb7
Leonardo Ferreira <leoalmeida.rj@gmail.com>
2025-07-02 18:29:39 -0300

    Update db-conn.js

--- connection/db-conn.js
+++ connection/db-conn.js
@@ -6,8 +6,6 @@ async function connectToDatabase(DB_URL = process.env.DATABASE_URL) {
         return;
     }
     try {
-        //const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}?retryWrites=true&w=majority&appName=ClusterLeo`
-        //const url = `mongodb+srv://api-nodejs:8VNergbNwt06HL7R@clusterleo.sckgivp.mongodb.net/db?retryWrites=true&w=majority&appName=ClusterLeo`
         _connect(DB_URL);
         const conn = await _connect(DB_URL);
         console.log('Connected to Database successfully');
